276332d4f6e0c6a42502190c921c71d6a6f13d6e
James Hart <james.elliot.hart@gmail.com>
2024-11-22 09:42:57 -0600

    max lengths for validation

--- MusicDataService/Domain/Song.cs
+++ MusicDataService/Domain/Song.cs
@@ -2,6 +2,9 @@ namespace MusicDataService.Domain;
 
 public class Song(Guid id, string name, string artist, string album)
 {
+    public const int MAX_NAME_LENGTH = 100;
+    public const int MAX_ARTIST_LENGTH = 100;
+    public const int MAX_ALBUM_LENGTH = 100;
     public Guid Id { get; } = id;
     public string Name { get; private set; } = name;
     public string Artist { get; private set; } = artist;
