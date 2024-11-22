d92b9836985cc0968d6b4d677486876d130e8a99
James Hart <james.elliot.hart@gmail.com>
2024-11-22 09:43:21 -0600

    add dtos

---
+++ MusicDataService/Dtos/Records.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using MusicDataService.Domain;
+
+namespace MusicDataService.Dtos;
+
+public record SongDto(Guid Id, string Name, string Artist, string Album);
+
+public class AddSongDto(Guid Id, string Name, string Artist, string Album)
+{
+    [Required]
+    public Guid Id { get; } = Id;
+    [Required, MaxLength(Song.MAX_NAME_LENGTH)]
+    public string Name { get; } = Name;
+    [Required, MaxLength(Song.MAX_ARTIST_LENGTH)]
+    public string Artist { get; } = Artist;
+    [Required, MaxLength(Song.MAX_ALBUM_LENGTH)]
+    public string Album { get; } = Album;
+}
+
+public class UpdateSongDto(Guid Id, string Name, string Artist, string Album)
+{
+    [Required]
+    public Guid Id { get; } = Id;
+    [Required, MaxLength(Song.MAX_NAME_LENGTH)]
+    public string Name { get; } = Name;
+    [Required, MaxLength(Song.MAX_ARTIST_LENGTH)]
+    public string Artist { get; } = Artist;
+    [Required, MaxLength(Song.MAX_ALBUM_LENGTH)]
+    public string Album { get; } = Album;
+}
