09c0b8cb61bfe51982a68191f4ac56b43d665333
James Hart <james.elliot.hart@gmail.com>
2024-11-23 15:37:07 -0600

    update favicon and attribute in footer

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -13,6 +13,15 @@ function App() {
       <body>
         <SongGrid songs={defaultSongs} />
       </body>
+      <footer className="App-footer">
+        <a
+          className="App-link"
+          href="https://icons8.com/icon/aKInTbkNvWT1/music-library"
+          target="_blank"
+        >
+          Music Library
+        </a> icon by <a className="App-link" target="_blank" href="https://icons8.com">Icons8</a>
+      </footer>
     </div>
   );
 }
