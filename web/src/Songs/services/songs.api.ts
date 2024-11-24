00aae3076f79bf587dbdac172ad166aa25df1c9d
James Hart <james.elliot.hart@gmail.com>
2024-11-23 21:32:54 -0600

    placeholder api calling code

---
+++ web/src/Songs/services/songs.api.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { ISong, UUIDv4 } from "../models/song.types";
+import { defaultSongs } from "../components/SongGrid";
+import { Result } from "../../shared/result";
+
+const api = axios.create({
+  baseURL: '',
+});
+
+export const fetchSongs = async () => {
+  try {
+    console.log('Fetching all songs')
+    // const response = await api.get<ISong[]>('/songs');
+    // console.log(response.data);
+
+    const data = Promise.resolve(defaultSongs);
+    const songData = await data;
+    return Result.Ok(songData);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return Result.Error(error.message);
+      // console.error('Axios error:', error.response?.status); // HTTP status code
+      // console.error('Error message:', error.message); // Error message
+    } else {
+      return Result.Error(error);
+    }
+  }
+};
+
+export const deleteSong = async (id: UUIDv4) => {
+  console.log(`Deleting song id '${id}'`);
+  // const response = await axios.delete(`/songs/${id}`);
+  const result = Promise.resolve(Result.Ok(1));
+  return result;
+}
