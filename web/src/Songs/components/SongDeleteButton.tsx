42ca2c8a8d0b48a150e810f4f9784e4f675a2718
James Hart <james.elliot.hart@gmail.com>
2024-11-23 21:39:07 -0600

    better naming

--- web/src/Songs/components/SongDeleteButton.tsx
+++ web/src/Songs/components/SongDeleteButton.tsx
@@ -2,8 +2,8 @@ import { ICellRendererParams } from "ag-grid-community";
 import { ISong } from "../models/song.types";
 import { SongGridRowClickHandler } from "../models/song-grid.types";
 
-export const SongDeleteButton = (p: ICellRendererParams<ISong>, handleClick: SongGridRowClickHandler) => {
+export const SongDeleteButton = (p: ICellRendererParams<ISong>, onDelete: SongGridRowClickHandler) => {
     return <>
-        <button onClick={() => handleClick(p)}>--</button>
+        <button onClick={() => onDelete(p)}>--</button>
     </>;
 };
