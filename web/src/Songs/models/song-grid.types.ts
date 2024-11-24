7eb5115dce29a497b470b21bbfd672ace9da1a51
James Hart <james.elliot.hart@gmail.com>
2024-11-23 21:33:34 -0600

    update signature to account for api calls

--- web/src/Songs/models/song-grid.types.ts
+++ web/src/Songs/models/song-grid.types.ts
@@ -1,4 +1,4 @@
 import { ICellRendererParams } from "ag-grid-community";
 import { ISong } from "./song.types";
 
-export type SongGridRowClickHandler = (params: ICellRendererParams<ISong>) => void;
+export type SongGridRowClickHandler = (params: ICellRendererParams<ISong>) => Promise<void>;
