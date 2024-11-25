2f6674372ec4c78bf4aaf0ce5cca515281770a34
James Hart <james.elliot.hart@gmail.com>
2024-11-25 10:58:07 -0600

    migration to create db and song table
    created with dotnet ef migrations add InitDb

---
+++ MusicDataService/Migrations/20241125053246_InitDb.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicDataService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "songs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Artist = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Album = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_songs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "songs");
+        }
+    }
+}
