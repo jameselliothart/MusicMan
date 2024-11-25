edf1293e3459d9bb37d1fb7715926e80f93f764b
James Hart <james.elliot.hart@gmail.com>
2024-11-25 14:37:57 -0600

    first test

---
+++ MusicDataService.Tests/SqlCommandHandlerShould.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using MusicDataService.CommandHandlers;
+using MusicDataService.Persistence;
+using MusicDataService.Songs;
+using MusicDataService.Songs.Commands;
+
+namespace MusicDataService.Tests;
+
+public class SqlCommandHandlerShould
+{
+    [Fact]
+    public async Task IndicateFailureIfSongAlreadyExists()
+    {
+        var song = new Song(Guid.NewGuid(), "name", "artist", "album");
+        var repoMock = new Mock<IMusicManRepository>();
+        repoMock
+            .Setup(x => x.Add(song))
+            .ReturnsAsync(0)
+        ;
+
+        var handler = new SqlCommandHandler(repoMock.Object);
+        var command = new AddSongCommand(song.Id, "name", "artist", "album");
+
+        var result = await handler.Handle(command);
+
+        Assert.Equal(0, result);
+    }
+}
