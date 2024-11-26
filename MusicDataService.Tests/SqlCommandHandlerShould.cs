4275aae1b67acea68f7d8b60693e8a99c11b1d0d
James Hart <james.elliot.hart@gmail.com>
2024-11-26 07:55:54 -0600

    fix test

--- MusicDataService.Tests/SqlCommandHandlerShould.cs
+++ MusicDataService.Tests/SqlCommandHandlerShould.cs
@@ -1,28 +1,28 @@
 using Moq;
-using MusicDataService.CommandHandlers;
 using MusicDataService.Persistence;
 using MusicDataService.Songs;
 using MusicDataService.Songs.Commands;
+using MusicDataService.Utils;
 
 namespace MusicDataService.Tests;
 
 public class SqlCommandHandlerShould
 {
     [Fact]
-    public async Task IndicateFailureIfSongAlreadyExists()
+    public async Task IndicateFailureIfDuplicateSongIsAdded()
     {
         var song = new Song(Guid.NewGuid(), "name", "artist", "album");
         var repoMock = new Mock<IMusicManRepository>();
         repoMock
             .Setup(x => x.Add(song))
-            .ReturnsAsync(0)
+            .ReturnsAsync(Result.Failure<int>(Error.Duplicate()))
         ;
 
         var handler = new SqlCommandHandler(repoMock.Object);
         var command = new AddSongCommand(song.Id, "name", "artist", "album");
 
-        var result = await handler.Handle(command);
+        var result = await handler.HandleAsync(command);
 
-        Assert.Equal(0, result);
+        Assert.Equal(Error.DUPLICATE, result.Error.Code);
     }
 }
