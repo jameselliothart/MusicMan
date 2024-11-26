1cc45263d9269211f0b7d0e8ff7e9c42dd5c936b
James Hart <james.elliot.hart@gmail.com>
2024-11-26 07:55:44 -0600

    implement equals for Song by id
    only used for tests right now but not a bad idea in general for Domain entities

--- MusicDataService/Songs/Song.cs
+++ MusicDataService/Songs/Song.cs
@@ -9,4 +9,26 @@ public class Song(Guid id, string name, string artist, string album)
     public string Name { get; private set; } = name;
     public string Artist { get; private set; } = artist;
     public string Album { get; private set; } = album;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Song other)
+            return false;
+
+        if (ReferenceEquals(this, other))
+            return true;
+
+        if (GetType() != other.GetType())
+            return false;
+
+        if (Id == Guid.Empty || other.Id == Guid.Empty)
+            return false;
+
+        return Id == other.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.ToString().GetHashCode();
+    }
 }
